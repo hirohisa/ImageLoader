15ff058f2e9f955e11cc61efee32dced52a21390
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-02-06 13:36:13 +0900

    Refactor swizzling method

--- ImageLoader/UIImageView+ImageLoader.m
+++ ImageLoader/UIImageView+ImageLoader.m
@@ -68,11 +68,7 @@ void ILSwizzleInstanceMethod(Class c, SEL original, SEL alternative)
 {
     Method orgMethod = class_getInstanceMethod(c, original);
     Method altMethod = class_getInstanceMethod(c, alternative);
-    if(class_addMethod(c, original, method_getImplementation(altMethod), method_getTypeEncoding(altMethod))) {
-        class_replaceMethod(c, alternative, method_getImplementation(orgMethod), method_getTypeEncoding(orgMethod));
-    } else {
-        method_exchangeImplementations(orgMethod, altMethod);
-    }
+    method_exchangeImplementations(orgMethod, altMethod);
 }
 
 @implementation UIImageView (ImageLoader)
